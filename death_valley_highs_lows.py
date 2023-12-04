11e13d8d30ba009cf8f80db9074724d9080a8ad6
Pedro Rodrigues <pedrolrodrigues3@gmail.com>
2023-12-04 11:15:58 -0300

    finishing project

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -3,19 +3,18 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-FILENAME = 'data/sitka_weather_2018_simple.csv'
+FILENAME = 'data/death_valley_2018_simple.csv'
 
 with open(FILENAME) as file:
     reader = csv.reader(file)
     header_row = next(reader)
 
-    # Get dates, highs and lows from the file
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         try:
-            high = int(row[5])
-            low = int(row[6])
+            high = int(row[4])
+            low =  int(row[5])
         except ValueError:
             continue
         else:
@@ -23,18 +22,16 @@ with open(FILENAME) as file:
             highs.append(high)
             lows.append(low)
 
-    # Plot the highs and lows
     plt.style.use(style='default')
     fig, ax = plt.subplots()
     ax.plot(dates, highs, c='red', alpha=0.5)
     ax.plot(dates, lows, c='blue', alpha=0.5)
     ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
-    # Format plot
-    ax.set_title('Daily highs and lows temperatures - 2018', fontsize=24)
+    ax.set_title('Daily highs and lows - 2018\nDeath Valley, CA', fontsize=20)
     ax.set_xlabel('', fontsize=16)
     fig.autofmt_xdate()
     ax.set_ylabel('Temperature (F)', fontsize=16)
     ax.tick_params(axis='both', which='major', labelsize=16)
 
-    plt.savefig('img/sitka_highs_lows_temperature.png', bbox_inches='tight')
+    plt.savefig('img/death_valley_highs_lows.png', bbox_inches='tight')
